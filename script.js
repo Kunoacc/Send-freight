136c5f7310a600d4e4730d1026ee3cf487eea428
ifetreaty <68975552+ifetreaty@users.noreply.github.com>
2021-10-11 20:08:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 function changeimg() {
     const img = document.getElementById('toggle--on').src;
     if (img.indexOf('toggle-on.png')!=-1) {
-        document.getElementById('toggle--on').src = 'toggle-off.png';
+        document.getElementById('toggle--on').src = 'img/toggle-off.png';
     } else {
-        document.getElementById('toggle--on').src = 'toggle-on.png';
+        document.getElementById('toggle--on').src = 'img/toggle-on.png';
     }
 }
 
 function changeback() {
     const img = document.getElementById('toggle--off').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--off').src = 'toggle-on.png';
+        document.getElementById('toggle--off').src = 'img/toggle-on.png';
     } else {
-        document.getElementById('toggle--off').src = 'toggle-off.png';
+        document.getElementById('toggle--off').src = 'img/toggle-off.png';
     }
 }
 
 function changebac() {
     const img = document.getElementById('toggle--of').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--of').src = 'toggle-on.png';
+        document.getElementById('toggle--of').src = 'img/toggle-on.png';
     } else {
-        document.getElementById('toggle--of').src = 'toggle-off.png';
+        document.getElementById('toggle--of').src = 'img/toggle-off.png';
     }
 }
 
 function changebak() {
     const img = document.getElementById('toggle--ff').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--ff').src = 'toggle-on.png';
+        document.getElementById('toggle--ff').src = 'img/toggle-on.png';
     } else {
-        document.getElementById('toggle--ff').src = 'toggle-off.png';
+        document.getElementById('toggle--ff').src = 'img/toggle-off.png';
     }
 }
 
 function changeba() {
     const img = document.getElementById('toggle--').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--').src = 'toggle-on.png';
+        document.getElementById('toggle--').src = 'img/toggle-on.png';
     } else {
-        document.getElementById('toggle--').src = 'toggle-off.png';
+        document.getElementById('toggle--').src = 'img/toggle-off.png';
     }
 }
 
