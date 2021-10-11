40250aafb6d74a4d2ce9a66c9230c670d9377819
ifetreaty <68975552+ifetreaty@users.noreply.github.com>
2021-10-11 20:14:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function changeimg() {
     const img = document.getElementById('toggle--on').src;
     if (img.indexOf('toggle-on.png')!=-1) {
-        document.getElementById('toggle--on').src = 'toggle-off.png';
+        document.getElementById('toggle--on').src = 'toggle-off.PNG';
     } else {
-        document.getElementById('toggle--on').src = 'toggle-on.png';
+        document.getElementById('toggle--on').src = 'toggle-on.PNG';
     }
 }
 
 function changeback() {
     const img = document.getElementById('toggle--off').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--off').src = 'toggle-on.png';
+        document.getElementById('toggle--off').src = 'toggle-on.PNG';
     } else {
-        document.getElementById('toggle--off').src = 'toggle-off.png';
+        document.getElementById('toggle--off').src = 'toggle-off.PNG';
     }
 }
 
 function changebac() {
     const img = document.getElementById('toggle--of').src;
     if (img.indexOf('toggle-off.png')!=-1) {
-        document.getElementById('toggle--of').src = 'toggle-on.png';
+        document.getElementById('toggle--of').src = 'toggle-on.PNG';
     } else {
-        document.getElementById('toggle--of').src = 'toggle-off.png';
+        document.getElementById('toggle--of').src = 'toggle-off.PNG';
     }
 }
 
 function changebak() {
     const img = document.getElementById('toggle--ff').src;
-    if (img.indexOf('toggle-off.png')!=-1) {
+    if (img.indexOf('toggle-off.PNG')!=-1) {
         document.getElementById('toggle--ff').src = 'toggle-on.PNG';
     } else {
         document.getElementById('toggle--ff').src = 'toggle-off.PNG';
